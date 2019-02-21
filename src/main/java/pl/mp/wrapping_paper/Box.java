6aad030a42ef4b360ce2d73d3686471864cfa64d
martaprosniak <martaprosniak@gmail.com>
2019-02-21 20:31:22 +0100

    Box class

---
+++ src/main/java/pl/mp/wrapping_paper/Box.java
@@ -0,0 +1,52 @@
+package pl.mp.wrapping_paper;
+
+public class Box {
+
+    private int length;
+
+    private int width;
+
+    private int height;
+
+    public Box() {
+    }
+
+    public Box(int length, int width, int height) {
+        this.length = length;
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "Box{" +
+                "length=" + length +
+                ", width=" + width +
+                ", height=" + height +
+                '}';
+    }
+}
