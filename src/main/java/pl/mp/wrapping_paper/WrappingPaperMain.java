4ac68773877b991722c4b15b96515070b95062e8
martaprosniak <martaprosniak@gmail.com>
2019-02-21 20:37:13 +0100

    Main class, boxes file

---
+++ src/main/java/pl/mp/wrapping_paper/WrappingPaperMain.java
@@ -0,0 +1,16 @@
+package pl.mp.wrapping_paper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WrappingPaperMain {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static List<Box> loadBoxes(){
+        List<Box> boxList = new ArrayList<>();
+        return boxList;
+    }
+}
