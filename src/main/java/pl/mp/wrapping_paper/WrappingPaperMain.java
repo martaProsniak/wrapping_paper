b5a2c09e8e412f4607572968b76173be859fac80
martaprosniak <martaprosniak@gmail.com>
2019-02-22 19:43:50 +0100

    Exercise solved

--- src/main/java/pl/mp/wrapping_paper/WrappingPaperMain.java
+++ src/main/java/pl/mp/wrapping_paper/WrappingPaperMain.java
@@ -5,21 +5,21 @@ import java.io.*;
 public class WrappingPaperMain {
 
     public static void main(String[] args) throws IOException {
-        System.out.println(calculate());
+        System.out.println("Paper needed to pack all boxes (dm): " + sumAll());
     }
 
-    public static int calculate() throws IOException {
+    public static int sumAll() throws IOException {
         String fileName = "src/boxes";
         FileReader fr = new FileReader(fileName);
         BufferedReader br = new BufferedReader(fr);
 
         String line;
-        int sumOfPaper = 0;
+        int sumPaper = 0;
         while ((line = br.readLine()) != null) {
             int paperForBox = calculatePaperForBox(line);
-            sumOfPaper = sumOfPaper + paperForBox;
+            sumPaper = sumPaper + paperForBox;
         }
-        return sumOfPaper;
+        return sumPaper;
     }
 
     public static int calculatePaperForBox(String line) {
@@ -28,40 +28,18 @@ public class WrappingPaperMain {
         int second = Integer.parseInt(sizeStr[1]);
         int third = Integer.parseInt(sizeStr[2]);
 
-        int min = 0;
-        int mid = 0;
-        int max = 0;
+        int min = Math.min(Math.min(first, second), third);
+        int max = Math.max(Math.max(first, second), third);
+        int mid;
 
-        if ((first <= second) && (first <= third)) {
-            min = first;
-            if (second <= third) {
-                mid = second;
-                max = third;
-            } else {
-                mid = third;
-                max = second;
-            }
-        } else if ((second <= first) && (second <= third)) {
-            min = second;
-            if (first <= third) {
-                mid = first;
-                max = third;
-            } else {
-                mid = third;
-                max = first;
-            }
-        } else if ((third <= first) && (third <= second)) {
-            min = third;
-            if (first <= second) {
-                mid = first;
-                max = second;
-            } else {
-                mid = second;
-                max = first;
-            }
+        if ((!(min == first) && !(max == first)) || ((first == second) || (first == third))) {
+            mid = first;
+        } else if ((!(min == second) && !(max == second)) || ((second == third))) {
+            mid = second;
+        } else {
+            mid = third;
         }
 
-        int paperForBox = (2 * min * mid) + (2 * mid * max) + (2 * max * min) + min;
-        return paperForBox;
+        return (3 * (min * mid)) + (2 * (mid * max)) + (2 * (max * min));
     }
 }
